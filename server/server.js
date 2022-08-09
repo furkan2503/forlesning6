b9da1154d902b573d4c28edc88c95bb6e99c605c
Furkan <fantastisk-4@hotmail.com>
2022-08-09 05:40:23 +0200

    ready for heruko

--- server/server.js
+++ server/server.js
@@ -2,6 +2,13 @@ import express from "express";
 
 const app = express();
 
+app.get("/api/login", (reg, res) => {
+
+    res.json({
+        username: "admin",
+        fullName: "Noen andre person",
+    })
+});
 const server = app.listen(process.env.PORT || 3000, () => {
-    console.log(`Server started on http://localhost:${server.address().port}`)
-})
+  console.log(`Server started on http://localhost:${server.address().port}`);
+});
